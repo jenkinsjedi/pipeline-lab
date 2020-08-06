7a959835d1dd1d7c417714dded966d1e53f85c9a
jenkinsjedi <69208714+jenkinsjedi@users.noreply.github.com>
2020-08-06 11:56:43 +0200

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-env
+sleep 15
+echo "mvn deploy"
 
-echo "Deploying: $1"
+echo "[INFO] Deploying...
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS \n [INFO] ------------------------------------------------------------------------
+[INFO] Total time: 0.087s
+[INFO] Finished at: Thu Aug 06 10:20:16 CEST 2020
+[INFO] Final Memory: 6M/225M"

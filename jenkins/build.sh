fdd90c87713f80e9948d5fe44198a6681c6cc14b
jenkinsjedi <69208714+jenkinsjedi@users.noreply.github.com>
2020-08-06 10:45:17 +0200

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,5 +3,9 @@
 sleep 15
 echo "mvn -B -DskipTests clean package"
 
-echo "[INFO] Scanning for projects...\n [INFO] ------------------------------------------------------------------------ \n [INFO] BUILD SUCCESS \n [INFO] ------------------------------------------------------------------------ \n [INFO] Total time: 0.087s \n [INFO] Finished at: Thu Aug 06 10:20:16 CEST 2020 \n [INFO] Final Memory: 6M/225M"
-
+echo "[INFO] Scanning for projects...
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS \n [INFO] ------------------------------------------------------------------------
+[INFO] Total time: 0.087s
+[INFO] Finished at: Thu Aug 06 10:20:16 CEST 2020
+[INFO] Final Memory: 6M/225M"

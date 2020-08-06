b1be8797db8d0ecf27ca2f6f23a6123f1aa5cbe4
jenkinsjedi <69208714+jenkinsjedi@users.noreply.github.com>
2020-08-06 10:38:15 +0200

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-export MAVEN_HOME="/usr/share/maven:$PATH"
-mvn -B -DskipTests clean package
+sleep 15
+echo "mvn -B -DskipTests clean package"
+
+echo "[INFO] Scanning for projects...\n [INFO] ------------------------------------------------------------------------ \n [INFO] BUILD SUCCESS \n [INFO] ------------------------------------------------------------------------ \n [INFO] Total time: 0.087s \n [INFO] Finished at: Thu Aug 06 10:20:16 CEST 2020 \n [INFO] Final Memory: 6M/225M"
